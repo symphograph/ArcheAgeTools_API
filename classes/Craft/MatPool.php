e8a0b3b9f6b3084be44e3945dd41ae25883ff588
symphograph <58216294+symphograph@users.noreply.github.com>
2023-01-08 06:25:33 +1100

    fix MatPool

--- classes/Craft/MatPool.php
+++ classes/Craft/MatPool.php
@@ -2,12 +2,16 @@
 
 namespace Craft;
 
+use PDO;
+
 class MatPool
 {
     /**
      * @var array<Mat>
      */
     public array $matPool = [];
+    private array $solidGroup = [];
+    private const solidGroupId = 1023;
 
     /**
      * @param int $resultItemId
@@ -16,22 +20,33 @@ class MatPool
     public static function getMatPool(int $resultItemId): array
     {
         $pool = new self();
+        $pool->initSolidGroup();
         $pool->initMatPool($resultItemId);
-        $pool->initMatPrices();
         return $pool->matPool;
     }
 
-    private function initMatPool(int $resultItemId): void
+    private function initMatPool(int $resultItemId, float $parentNeed = 1): void
     {
         $craft = CraftPool::getPool($resultItemId)->mainCraft;
+
         foreach ($craft->Mats as $mat){
-            if(empty($this->matPool[$mat->id])){
-                $this->matPool[$mat->id] = $mat;
+            if($mat->need < 0)
+                continue;
+
+            $mat->initPrice();
+            $mat->Price->initAuthor();
+
+            if($mat->craftable && !$mat->isBuyOnly && !in_array($mat->Item->categId, $this->solidGroup)){
+                self::initMatPool($mat->id, $mat->need * $parentNeed / $craft->resultAmount);
+                continue;
             }
-            if($mat->craftable){
-                self::initMatPool($mat->id);
+            if(!empty($this->matPool[$mat->id])){
+                $mat->need += $mat->need * $parentNeed / $craft->resultAmount;
+            }else{
+                $mat->need = $mat->need * $parentNeed / $craft->resultAmount;
             }
-            $this->matPool[$mat->id]->need += $mat->need;
+            $this->matPool[$mat->id] = $mat;
+
         }
     }
 
@@ -44,5 +59,15 @@ class MatPool
             $mats[] = $mat;
         }
         $this->matPool = $mats;
+    }
+
+    private function initSolidGroup(): void
+    {
+        $qwe = qwe("
+            select id from Categories 
+            where parent = :parent",
+            ['parent'=> self::solidGroupId]
+        );
+        $this->solidGroup = $qwe->fetchAll(PDO::FETCH_COLUMN);
     }
 }
