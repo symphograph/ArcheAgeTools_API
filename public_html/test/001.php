5334281769576867746dfd4c6a2d23c86fcfdf94
symphograph <58216294+symphograph@users.noreply.github.com>
2023-09-18 15:46:07 +1100

    speedTest

--- public_html/test/001.php
+++ public_html/test/001.php
@@ -29,7 +29,7 @@ use Symphograph\Bicycle\SQL\SQLBuilder;
 $AccSets = \App\User\AccSettings::byIdAndInit(1057);
 
 $Test = new App\Test\Test();
-echo 'TestMedian: ' . $Test->speedTestTime('sqlBenchMark', 100);
+echo 'TestMedian: ' . $Test->speedTestTime('sqlBenchMark', 1000);
 
 echo Test::scriptTime($start);
 
